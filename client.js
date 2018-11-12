ab007bdd9c9bcae397c51dd1893ef0a14e40baec
Ryan Mundy <ryanmundy34@gmail.com>
2018-11-12 14:02:34 -0600

    background color changes to red when clicked

--- client.js
+++ client.js
@@ -2,18 +2,21 @@ console.log('js is loaded');
 
 $(document).ready(readyNow);
 
-let counter = 0;
-
 function readyNow(){
     console.log('jQuery is loaded');
     $('#generateButton').on('click', buttonClicked);
 }//end readyNow
 
+let counter = 0;
+
 function buttonClicked() {
     console.log('generate button clicked');
     $('.newDiv').empty();
+    $('.newDiv').css('background-color', 'red');//makes background red on generate button click
     counter++;//increments each time button is clicked
     $('.newDiv').append(`<div class="newClass"></div>`);//creates new div element on DOM
     $('.newClass').append(`<p class="newP">${counter}</p>`);//creates new p tag
-
-}
+    $('.newClass').append(`<button type="button" id="swapButton">Swap</button>`);//creates swap button
+    $('.newClass').append(`<button type="button" id="deleteButton">Delete</button>`);//creates delete button
+    
+}// end button clicked
