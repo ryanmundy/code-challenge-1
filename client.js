6d7ab57a72e9e2163d6892ee390edc38a1336322
Ryan Mundy <ryanmundy34@gmail.com>
2018-11-12 13:29:15 -0600

    created counter variable, and appended new div and p tag

--- client.js
+++ client.js
@@ -2,6 +2,8 @@ console.log('js is loaded');
 
 $(document).ready(readyNow);
 
+let counter = 0;
+
 function readyNow(){
     console.log('jQuery is loaded');
     $('#generateButton').on('click', buttonClicked);
@@ -9,5 +11,8 @@ function readyNow(){
 
 function buttonClicked() {
     console.log('generate button clicked');
-    
+    counter++;//increments each time button is clicked
+    $('.bodyClass').append(`<div class="newClass"></div>`);//creates new div element on DOM
+    $('.newClass').append(`<p class="newP">new p tag</p>`);//creates new p tag
+
 }
