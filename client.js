9863781cdc225ca2ff25df38c77e15d3fb811359
Ryan Mundy <ryanmundy34@gmail.com>
2018-11-12 13:18:12 -0600

    generate button added

--- client.js
+++ client.js
@@ -4,5 +4,10 @@ $(document).ready(readyNow);
 
 function readyNow(){
     console.log('jQuery is loaded');
+    $('#generateButton').on('click', buttonClicked);
+}//end readyNow
+
+function buttonClicked() {
+    console.log('generate button clicked');
     
 }
